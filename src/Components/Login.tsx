fe6a168d623e5fb0fc7aa71d4803de450d9c2b98
RamezYoussef712 <ramez.youssef.fahmy@gmail.com>
2022-07-06 15:00:29 +0200

    6/7

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,16 +1,15 @@
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import TextField from '@mui/material/TextField/TextField';
-import Button from '@mui/material/Button';
-import { useState } from 'react';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import TextField from "@mui/material/TextField/TextField";
+import Button from "@mui/material/Button";
+import { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPass] = useState("");
 
-  const [email, setEmail] = useState("")
-  const [password, setPass] = useState("")
-
-  async function sendData(e:any) {
+  async function sendData(e: any) {
     e.preventDefault();
     let checkSubmit = true;
 
@@ -23,61 +22,71 @@ function Login() {
     }
 
     if (checkSubmit) {
-      
-      try{
-        const response = await fetch ('http://localhost:4000/auth/login', {
+      try {
+        const response = await fetch("http://localhost:4000/auth/login", {
           method: "POST",
-          headers: {"content-Type":"application/json"},
-          body: JSON.stringify({email: email, password: password})
+          headers: { "content-Type": "application/json" },
+          body: JSON.stringify({ email, password }),
         });
 
-        if (response.ok){
-          // console.log(response.status);
-          // console.log('loggind done');
-          let USER_TOKEN = await response.text();
-          let token =  JSON.parse(USER_TOKEN).token;
-          localStorage.setItem('TOKEN', token);
+        if (response.ok) {
+          console.log(response.status);
+          console.log('logged in successfully');
+          const USER_TOKEN = await response.text();
+          const token = JSON.parse(USER_TOKEN).token;
+          localStorage.setItem("TOKEN", token);
         }
-
-      }catch(error){
-       console.log(error);
+      } catch (error) {
+        console.log(error);
       }
     }
   }
 
   return (
     <div className="container">
-
       <div>
-        <Card sx={{maxWidth: 700}} style={{minHeight: '150vh'}}>
+        <Card sx={{ maxWidth: 700 }} style={{ minHeight: "150vh" }}>
           <form onSubmit={sendData}>
-
             <CardContent>
               <h2>Log In</h2>
-              
+
               <div>
-                <TextField required fullWidth id="name" label="username" onChange={(e) => {
-                  setEmail(e.target.value)
-                }}/>
-              </div><br/>
-              
+                <TextField
+                  required
+                  fullWidth
+                  id="name"
+                  label="email"
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
+              </div>
+              <br />
+
               <div>
-                <TextField required fullWidth id="password" type="password" label="password" onChange={(e) => {
-                  setPass(e.target.value)
-                }}/>
+                <TextField
+                  required
+                  fullWidth
+                  id="password"
+                  type="password"
+                  label="password"
+                  onChange={(e) => {
+                    setPass(e.target.value);
+                  }}
+                />
               </div>
             </CardContent>
 
             <CardActions>
-              <Button variant="contained" type="submit">Log In</Button>
+              <Button variant="contained" type="submit">
+                Log In
+              </Button>
             </CardActions>
-
           </form>
         </Card>
       </div>
-
     </div>
-  )
+  );
 }
 
 export default Login;
--- src/Components/VerifyCode.tsx
+++ src/Components/VerifyCode.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 import TextField from '@mui/material/TextField/TextField';
 import Button from '@mui/material/Button';
 import { Box } from "@mui/system";
+import { useNavigate } from "react-router-dom";
 
 function VerifyCode() {
-
+    const navigate = useNavigate();
     const [email, setEmail] = useState("");
     const [code, setCode] = useState("");
 
@@ -14,15 +15,16 @@ function VerifyCode() {
         console.log("Send Data");
 
         try {
-            const response = await fetch('http://localhost:8000/auth/verify_code', {
+            const response = await fetch('http://localhost:4000/auth/verify_code', {
                 method: "POST",
                 headers: { "content-Type": "application/json" },
-                body: JSON.stringify({ email: email, code: code })
+                body: JSON.stringify({ email, code })
             });
 
             if (response.ok) {
                 console.log(response.status);
                 console.log('Code verified');
+                navigate('/reset_password', );
             }
 
         } catch (error) {
