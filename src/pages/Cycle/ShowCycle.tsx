4d7d581ce2b44b4b03ae97792d24c101ee02c5fe
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2022-07-08 16:25:34 +0200

    refactor show cycle

---
+++ src/pages/Cycle/ShowCycle.tsx
@@ -0,0 +1,35 @@
+import ShowCycleComponent from '../../components/cycles/show'
+import {useEffect, useState} from "react";
+import {ICycleInterface} from "../../config/interfaces/ICycle.interface";
+import {useParams} from "react-router-dom";
+import {IResponseInterface} from "../../config/interfaces/IResponse.interface";
+import api from "../../config/api";
+
+const ShowCycle = () => {
+	const [cycle, setCycle] = useState<ICycleInterface>()
+	const { id } = useParams()
+	const getCycle = async () => {
+		try {
+			const response: IResponseInterface<ICycleInterface> =
+				await api<ICycleInterface>({
+					url: `/cycles/show/${id}`,
+				})
+
+			if (response.success) {
+				if (response.data) {
+					setCycle(response.data)
+					console.log(response.data)
+				}
+			}
+		} catch (error: any) {
+			console.log(error)
+		}
+	}
+	useEffect(() => {
+		getCycle()
+	}, [])
+	return(
+     <ShowCycleComponent cycle={cycle}/>
+	)
+}
+export default ShowCycle
